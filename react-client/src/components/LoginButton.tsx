036d62a8a7fd6ede6eeb6aadd53673e272d9c4f3
Sankalp Patil <patilsa@oregonstate.edu>
2023-07-22 20:17:41 -0700

    Help and userlogin buttons

---
+++ react-client/src/components/LoginButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "@aws-amplify/ui-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons"; // Import the required icon
+
+interface LoginButtonProps {
+  color: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export function LoginButton({ color, onClick }: LoginButtonProps) {
+  return (
+    <Button
+      position="absolute"
+      top="40px" /* Slightly inwards from the bottom */
+      right="40px" /* Slightly inwards from the right */
+
+      backgroundColor={color}
+      borderRadius="50%" /* To make it perfectly round */
+
+      width="50px" /* Adjust the width to your desired size */
+      height="50px" /* Adjust the height to your desired size */
+
+      padding="1em"
+
+      border="none"
+      boxShadow="rgba(13, 26, 38, 0.25) 0px 4px 4px 0px"
+      onClick={onClick}
+    >
+      <FontAwesomeIcon icon={faUser} style={{ color: "#ffffff" }} />
+    </Button>
+  );
+}
--- react-client/src/pages/Landing.tsx
+++ react-client/src/pages/Landing.tsx
@@ -1,17 +1,24 @@
 import { ToggleButton } from "../components/Buttons";
 import { useNavigate } from "react-router-dom";
 import "@aws-amplify/ui-react/styles.css";
+import { HelpButton } from "../components/HelpButton";
+import { LoginButton } from "../components/LoginButton";
 
 const Landing = () => {
   const navigate = useNavigate();
   return (
     <>
+      <LoginButton color="#000000" onClick={() => navigate("login")}></LoginButton> 
+      <h1>ARE YOU SMARTER THAN AN AI?</h1>
       <ToggleButton color="#FF6DDF" onClick={() => navigate("create-game")}>
         Create a new game
       </ToggleButton>
       <ToggleButton color="#62A1FF" onClick={() => navigate("join-game")}>
         Join existing game
       </ToggleButton>
+      <HelpButton color="#000000" onClick={() => navigate("help")}>
+        ?
+      </HelpButton>
     </>
   );
 };
