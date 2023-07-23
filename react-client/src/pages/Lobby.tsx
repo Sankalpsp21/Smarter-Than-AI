6caf876534357cd7026c861010b2e1497d3a72db
ImgyeongLee <mingbab33@gmail.com>
2023-07-23 21:40:57 +0900

    finish implementing all pages briefly

--- react-client/src/pages/Lobby.tsx
+++ react-client/src/pages/Lobby.tsx
@@ -1,4 +1,43 @@
 import LoadingSpinner from "../components/LoadingSpinner";
+import { Text } from "@aws-amplify/ui-react";
+
+export function Waiting() {
+  return (
+    <>
+      <Text
+        marginBottom="1em"
+        fontSize="1.2em"
+        fontWeight="500"
+        alignSelf="center"
+      >
+        Waiting for Host to Start...
+      </Text>
+      <Text
+        marginBottom="2em"
+        fontStyle="normal"
+        textDecoration="none"
+        alignSelf="center"
+      >
+        Players joined: currentPlayerNum
+      </Text>
+    </>
+  );
+}
+
+export function Generating() {
+  return (
+    <>
+      <Text
+        marginBottom="3em"
+        fontSize="1.2em"
+        fontWeight="500"
+        alignSelf="center"
+      >
+        Generating Prompt...
+      </Text>
+    </>
+  );
+}
 
 const Lobby = () => {
   return (
