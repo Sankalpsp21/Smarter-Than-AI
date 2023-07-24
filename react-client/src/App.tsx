c0b4d6afc3cb8233ad3e1bf5bf5e12e1817edf57
Toby Loki <tobyloki5@gmail.com>
2023-07-23 18:25:51 -0700

    (1) started adding logic to Lobby, Message, Prompt (2) split Game.tsx into Play, Message, and Vote

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,46 +1,53 @@
-import Test from "./pages/Test";
-import Landing from "./pages/Landing";
-import CreateGame from "./pages/CreateGame";
-import JoinGame from "./pages/JoinGame";
-import Game from "./pages/Game";
-import Result from "./pages/Result";
-import Lobby from "./pages/Lobby";
-import { Amplify } from "aws-amplify";
-import { ThemeProvider, Theme } from "@aws-amplify/ui-react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "@aws-amplify/ui-react/styles.css";
+import Test from './pages/Test';
+import Landing from './pages/Landing';
+import CreateGame from './pages/CreateGame';
+import JoinGame from './pages/JoinGame';
+import Game from './pages/Vote';
+import Result from './pages/Result';
+import Lobby from './pages/Lobby';
+import Prompt from './pages/Prompt';
+import Message from './pages/Message';
+import Play from './pages/Play';
+import Vote from './pages/Vote';
+import { Amplify } from 'aws-amplify';
+import { ThemeProvider, Theme } from '@aws-amplify/ui-react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import '@aws-amplify/ui-react/styles.css';
 
 // Use next two lines only if syncing with the cloud
-import awsExports from "./aws-exports";
+import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 const theme: Theme = {
-  name: "my-theme",
-  tokens: {
-    colors: {
-      font: {
-        primary: { value: "#ffffff" },
-      },
-    },
-  },
+	name: 'my-theme',
+	tokens: {
+		colors: {
+			font: {
+				primary: { value: '#ffffff' }
+			}
+		}
+	}
 };
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/create-game" element={<CreateGame />} />
-            <Route path="/join-game" element={<JoinGame />} />
-            <Route path="/game" element={<Game />} />
-            <Route path="/result" element={<Result />} />
-            <Route path="/lobby" element={<Lobby />} />
-            <Route path="/test" element={<Test />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<div className="App">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Landing />} />
+						<Route path="/create-game" element={<CreateGame />} />
+						<Route path="/join-game" element={<JoinGame />} />
+						<Route path="/play" element={<Play />} />
+						<Route path="/vote" element={<Vote />} />
+						<Route path="/result" element={<Result />} />
+						<Route path="/prompt" element={<Prompt />} />
+						<Route path="/lobby" element={<Lobby />} />
+						<Route path="/message" element={<Message />} />
+						<Route path="/test" element={<Test />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</ThemeProvider>
+	);
 }
---
+++ react-client/src/pages/Play.tsx
@@ -0,0 +1,80 @@
+import { Button, Text, TextField, Flex } from '@aws-amplify/ui-react';
+import { ToggleButton } from '../components/Buttons';
+import { PromptCard, PinkCard, VoteCard } from '../components/Cards';
+import GameNavbar from '../components/GameNavbar';
+import { DataStore } from 'aws-amplify';
+import { useEffect, useState } from 'react';
+import { GameSession } from '../models';
+
+export function Play() {
+	const [currentResponededPlayer, setCurrentResponededPlayer] = useState(0);
+	const [currentRound, setCurrentRound] = useState(0);
+
+	useEffect(() => {
+		const fetch = async () => {
+			try {
+				const gameSessions = await DataStore.query(GameSession);
+				setCurrentResponededPlayer(gameSessions[0].playerCount);
+				setCurrentResponededPlayer(gameSessions[0].playersResponded);
+				setCurrentRound(gameSessions[0].roundNumber);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetch();
+	}, []);
+
+	return (
+		<>
+			<Text
+				variation="primary"
+				as="p"
+				lineHeight="1.5em"
+				fontWeight={500}
+				fontSize="1em"
+				fontStyle="normal"
+				textDecoration="none"
+				style={{ position: 'fixed', top: '5vh', cursor: 'default' }}
+			>
+				Players responded ({currentResponededPlayer}/5)
+			</Text>
+			<PinkCard
+				style={{ position: 'fixed', top: '10vh', cursor: 'default' }}
+			>
+				Round {currentRound} Prompt
+			</PinkCard>
+			<PromptCard>This is a round 1 question</PromptCard>
+			<Flex direction="row" justifyContent="center">
+				<TextField
+					label="Answer"
+					labelHidden
+					placeholder="Enter your response"
+					width="35vw"
+					backgroundColor={'transparent'}
+					alignSelf={'center'}
+					boxShadow={'rgba(13, 26, 38, 0.25) 0px 4px 4px 0px'}
+					inputStyles={{
+						color: '#000000',
+						backgroundColor: '#ffffff',
+						border: 'none',
+						borderRadius: '10px'
+					}}
+				></TextField>
+				<ToggleButton color="#FF6DDF">Submit</ToggleButton>
+			</Flex>
+		</>
+	);
+}
+
+const Game = () => {
+	return (
+		<>
+			<div style={{ width: '100%', position: 'fixed', top: '0' }}>
+				<GameNavbar />
+			</div>
+			<Play />
+		</>
+	);
+};
+
+export default Game;
