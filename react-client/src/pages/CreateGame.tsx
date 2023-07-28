1f6d3e14048f5e3dab5cc9c48a61f84143c38671
Toby Loki <tobyloki5@gmail.com>
2023-07-27 19:59:07 -0700

    CreateGame.tsx set gameSessionId & isHost for redux

--- react-client/src/pages/CreateGame.tsx
+++ react-client/src/pages/CreateGame.tsx
@@ -1,48 +1,53 @@
-import { Button, Text, Flex } from "@aws-amplify/ui-react";
-import { useEffect, useState, useRef } from "react";
-import { DataStore } from "aws-amplify";
-import { GameSession } from "../models";
-import { ToggleButton } from "../components/Buttons";
-import { LoginButton } from "../components/LoginButton";
-import { useNavigate } from "react-router-dom";
+import { Button, Text, Flex } from '@aws-amplify/ui-react';
+import { useEffect, useState, useRef } from 'react';
+import { DataStore } from 'aws-amplify';
+import { GameSession, RoundMode } from '../models';
+import { ToggleButton } from '../components/Buttons';
+import { LoginButton } from '../components/LoginButton';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setIsHost, setGameSessionID } from '../redux/GameSlice';
+import { AppDispatch } from '../redux/store';
 
 interface CopyToClipboard {
-  target: string;
+	target: string;
 }
 
 export const copyToClipboard = async ({ target }: CopyToClipboard) => {
-  try {
-    let value = "";
+	try {
+		let value = '';
 
-    if (!navigator.clipboard) {
-      throw new Error("Browser doesn't have support for native clipboard");
-    }
+		if (!navigator.clipboard) {
+			throw new Error(
+				"Browser doesn't have support for native clipboard"
+			);
+		}
 
-    if (target) {
-      const node = document.querySelector(target);
+		if (target) {
+			const node = document.querySelector(target);
 
-      if (!node || !node.textContent) {
-        throw new Error("Element not found");
-      }
+			if (!node || !node.textContent) {
+				throw new Error('Element not found');
+			}
 
-      value = node.textContent;
-    }
+			value = node.textContent;
+		}
 
-    if (!value) {
-      throw new Error("Failed to copy");
-    }
+		if (!value) {
+			throw new Error('Failed to copy');
+		}
 
-    await navigator.clipboard.writeText(value);
-    return true;
-  } catch (err) {
-    console.log(err);
-    return false;
-  }
+		await navigator.clipboard.writeText(value);
+		return true;
+	} catch (err) {
+		console.log(err);
+		return false;
+	}
 };
 
 const pinCodeExists = async (pinCode: number) => {
-  // GraphQL query
-  /* 
+	// GraphQL query
+	/* 
     query PinNumbersQuery {
       listGameSessions {
         items {
@@ -51,169 +56,179 @@ const pinCodeExists = async (pinCode: number) => {
       }
     }
   */
-  try {
-    return await DataStore.query(GameSession, (g) => g.pinCode.eq(pinCode));
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		return await DataStore.query(GameSession, (g) => g.pinCode.eq(pinCode));
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 const CreateGame = () => {
-  const [currentPlayerNum, setCurrentPlayerNum] = useState(0);
-  const playerCount = useRef(0);
-  const [pinCode, setPinCode] = useState(0);
-  const gameSessionId = useRef("");
-  const [copySuccess, setCopySuccess] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+	const dispatch: AppDispatch = useDispatch();
 
-  const navigate = useNavigate();
+	const [currentPlayerNum, setCurrentPlayerNum] = useState(1);
+	const playerCount = useRef(1);
+	const [pinCode, setPinCode] = useState(0);
+	const gameSessionId = useRef('');
+	const [copySuccess, setCopySuccess] = useState(false);
+	const [isError, setIsError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
 
-  const setupPinCode = async () => {
-    try {
-      // Generate a pin code
-      let pin = 0;
-      do {
-        pin = Math.floor(Math.random() * 8999) + 1000;
-      } while (!(await pinCodeExists(pin)));
-      console.log(`Pin: ${pin}`);
-      setPinCode(pin);
-      return pin;
-    } catch (error) {
-      console.error(error);
-    }
-    return -1;
-  };
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    const initGame = async () => {
-      try {
-        // Create a new GameSession
-        const gameSession = new GameSession({
-          pinCode: await setupPinCode(),
-          playerCount: 1,
-          roundNumber: 0,
-          roundPrompt: "",
-          currentRoundExpiration: new Date().toISOString(),
-          UserSessions: [],
-          playersResponded: 0,
-          roundMode: "PROMPT",
-          aiResponse: "",
-        });
+	const setupPinCode = async () => {
+		try {
+			// Generate a pin code
+			let pin = 0;
+			do {
+				pin = Math.floor(Math.random() * 8999) + 1000;
+			} while (!(await pinCodeExists(pin)));
+			console.log(`Pin: ${pin}`);
+			setPinCode(pin);
+			return pin;
+		} catch (error) {
+			console.error(error);
+		}
+		return -1;
+	};
 
-        console.log(`game session pin code: ${gameSession.pinCode}`);
+	useEffect(() => {
+		const initGame = async () => {
+			try {
+				// Create a new GameSession
+				const gameSession = new GameSession({
+					pinCode: await setupPinCode(),
+					playerCount: 1,
+					roundNumber: 0,
+					roundPrompt: '',
+					currentRoundExpiration: new Date().toISOString(),
+					UserSessions: [],
+					playersResponded: 0,
+					roundMode: RoundMode.PROMPT,
+					aiResponse: ''
+				});
 
-        // Save the GameSession to the database
-        await DataStore.save(gameSession);
+				console.log(`game session pin code: ${gameSession.pinCode}`);
 
-        // Set the page's gameSessionId from the id of the GameSession we just created
-        gameSessionId.current = gameSession.id;
-        console.log(`Game Session id: ${gameSession.id}`);
+				// Save the GameSession to the database
+				await DataStore.save(gameSession);
 
-        // Subscribe to updates to playerCount
-        const subscription = DataStore.observe(
-          GameSession,
-          gameSessionId.current
-        ).subscribe((msg: any) => {
-          //TODO: test this
-          const item = msg.element;
-          console.log("ITEM IS ====", item);
-          playerCount.current = item.playerCount;
-          setCurrentPlayerNum(item.playerCount);
-        });
-        console.log(subscription);
+				// Set the page's gameSessionId from the id of the GameSession we just created
+				gameSessionId.current = gameSession.id;
+				console.log(`Game Session id: ${gameSession.id}`);
 
-        // TODO: handle unsubscribe
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    initGame();
-  }, []);
+				dispatch(setGameSessionID(gameSession.id));
+				dispatch(setIsHost(true));
 
-  const getGameSession = async (pinCode: number) => {
-    try {
-      // get all game sessions
-      const gameSession = await DataStore.query(GameSession, (c) =>
-        c.and((c) => [c.pinCode.eq(pinCode)])
-      );
-      if (gameSession.length === 0) return null;
-      return gameSession[0];
-    } catch (error) {
-      console.error(error);
-      return null;
-    }
-  };
+				// Subscribe to updates to playerCount
+				const subscription = DataStore.observe(
+					GameSession,
+					gameSessionId.current
+				).subscribe((msg: any) => {
+					const item = msg.element;
+					console.log('ITEM IS ====', item);
+					playerCount.current = item.playerCount;
+					setCurrentPlayerNum(item.playerCount);
+				});
+				console.log(subscription);
 
-  const handleStartbtn = async () => {
-    try {
-      // Get game session
-      const gameSession = await getGameSession(pinCode);
+				// TODO: handle unsubscribe
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		initGame();
+	}, []);
 
-      // Handle invalid game session
-      if (gameSession == null || gameSession.roundNumber > 0) {
-        console.error("Invalid pin code or game session has already started");
-        setIsError(true);
-        setErrorMessage("Invalid pin code or game session has already started");
-        return;
-      } else if (playerCount.current < 2) {
-        setIsError(true);
-        setErrorMessage(
-          "You don't have enough number of players to start the game"
-        );
-        return;
-      }
+	const getGameSession = async (pinCode: number) => {
+		try {
+			// get all game sessions
+			const gameSession = await DataStore.query(GameSession, (c) =>
+				c.and((c) => [c.pinCode.eq(pinCode)])
+			);
+			if (gameSession.length === 0) return null;
+			return gameSession[0];
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	};
 
-      // Increment GameSession roundNumber by 1
-      await DataStore.save(
-        GameSession.copyOf(gameSession, (updated) => {
-          updated.roundNumber = gameSession.roundNumber + 1;
-        })
-      );
+	const handleStartbtn = async () => {
+		try {
+			// Get game session
+			const gameSession = await getGameSession(pinCode);
 
-      navigate("/prompt");
-    } catch (error) {
-      console.error(error);
-    }
-  };
+			// Handle invalid game session
+			if (gameSession == null || gameSession.roundNumber > 0) {
+				console.error(
+					'Invalid pin code or game session has already started'
+				);
+				setIsError(true);
+				setErrorMessage(
+					'Invalid pin code or game session has already started'
+				);
+				return;
+			} else if (playerCount.current < 2) {
+				setIsError(true);
+				setErrorMessage(
+					"You don't have enough number of players to start the game"
+				);
+				return;
+			}
 
-  return (
-    <>
-      <LoginButton color="#000000" />
-      <h1>ARE YOU SMARTER THAN AN AI?</h1>
-      <div>Tell your friends to join this pin code</div>
-      <Flex direction="row">
-        <Text
-          fontStyle="normal"
-          textDecoration="none"
-          alignSelf="center"
-          className="pincode"
-        >
-          {pinCode}
-        </Text>
-        <Button
-          onClick={async () => {
-            let result = await copyToClipboard({ target: ".pincode" });
-            setCopySuccess(result);
-            setTimeout(() => {
-              setCopySuccess(false);
-            }, 1000);
-          }}
-          disabled={copySuccess ? true : false}
-        >
-          {copySuccess ? "Copied!" : "Copy"}
-        </Button>
-      </Flex>
-      <Text fontStyle="normal" textDecoration="none" alignSelf="center">
-        Players joined: {currentPlayerNum}
-      </Text>
-      {isError && <Text color="#ffa6a6">{errorMessage}</Text>}
+			// Increment GameSession roundNumber by 1
+			await DataStore.save(
+				GameSession.copyOf(gameSession, (updated) => {
+					updated.roundNumber = gameSession.roundNumber + 1;
+				})
+			);
 
-      <ToggleButton color="#FF6DDF" onClick={handleStartbtn}>
-        Start Game
-      </ToggleButton>
-    </>
-  );
+			navigate('/prompt');
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return (
+		<>
+			<LoginButton color="#000000" />
+			<h1>ARE YOU SMARTER THAN AN AI?</h1>
+			<div>Tell your friends to join this pin code</div>
+			<Flex direction="row">
+				<Text
+					fontStyle="normal"
+					textDecoration="none"
+					alignSelf="center"
+					className="pincode"
+				>
+					{pinCode}
+				</Text>
+				<Button
+					onClick={async () => {
+						let result = await copyToClipboard({
+							target: '.pincode'
+						});
+						setCopySuccess(result);
+						setTimeout(() => {
+							setCopySuccess(false);
+						}, 1000);
+					}}
+					disabled={copySuccess ? true : false}
+				>
+					{copySuccess ? 'Copied!' : 'Copy'}
+				</Button>
+			</Flex>
+			<Text fontStyle="normal" textDecoration="none" alignSelf="center">
+				Players joined: {currentPlayerNum}
+			</Text>
+			{isError && <Text color="#ffa6a6">{errorMessage}</Text>}
+
+			<ToggleButton color="#FF6DDF" onClick={handleStartbtn}>
+				Start Game
+			</ToggleButton>
+		</>
+	);
 };
 
 export default CreateGame;
