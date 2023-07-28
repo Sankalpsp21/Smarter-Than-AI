6a09c5c128a4e13b5a2db23e4566cb6e8d125fee
Sankalp Patil <patilsa@oregonstate.edu>
2023-07-27 20:32:00 -0700

    Added timer in navbar

--- react-client/src/components/GameNavbar.tsx
+++ react-client/src/components/GameNavbar.tsx
@@ -1,6 +1,30 @@
 import { Button } from "@aws-amplify/ui-react";
+import React, { useEffect, useState } from "react";
 
 const GameNavbar = () => {
+  const [timer, setTimer] = useState(30);
+
+  useEffect(() => {
+    let interval;
+    if (timer > 0) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timer]);
+
+  const formatTimer = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
   return (
     <div
       style={{
@@ -11,7 +35,7 @@ const GameNavbar = () => {
         backgroundColor: "transparent",
       }}
     >
-      <div>00:30</div>
+      <div>{formatTimer(timer)}</div>
       <Button>Exit</Button>
     </div>
   );
