3c1aca411f98bfa3ec3ad6d9b21661fd1043a8d8
Toby Loki <tobyloki5@gmail.com>
2023-07-27 20:53:49 -0700

    JoinGame.tsx getGameSession() tries again after 0.5s to force refresh of data in underlying DataStore

--- react-client/src/pages/JoinGame.tsx
+++ react-client/src/pages/JoinGame.tsx
@@ -1,127 +1,135 @@
-import { TextField, Text } from "@aws-amplify/ui-react";
-import { ToggleButton } from "../components/Buttons";
-import { useEffect, useState } from "react";
-import { DataStore } from "aws-amplify";
-import { GameSession, UserSession } from "../models";
-import { LoginButton } from "../components/LoginButton";
-import { useDispatch, useSelector } from "react-redux";
+import { TextField, Text } from '@aws-amplify/ui-react';
+import { ToggleButton } from '../components/Buttons';
+import { useEffect, useState } from 'react';
+import { DataStore } from 'aws-amplify';
+import { GameSession, UserSession } from '../models';
+import { LoginButton } from '../components/LoginButton';
+import { useDispatch, useSelector } from 'react-redux';
 import {
-  selectUserSessionID,
-  setUserSessionID,
-  setGameSessionID,
-} from "../redux/GameSlice";
-import { AppDispatch } from "../redux/store";
-import { useNavigate } from "react-router-dom";
+	selectUserSessionID,
+	setUserSessionID,
+	setGameSessionID
+} from '../redux/GameSlice';
+import { AppDispatch } from '../redux/store';
+import { useNavigate } from 'react-router-dom';
 
 const JoinGame = () => {
-  const dispatch: AppDispatch = useDispatch();
-  const userSessionID = useSelector(selectUserSessionID);
-  const [pinCode, setPinCode] = useState<number>(0);
-  const [error, setError] = useState<string>("");
-  const navigate = useNavigate();
+	const dispatch: AppDispatch = useDispatch();
+	const userSessionID = useSelector(selectUserSessionID);
+	const [pinCode, setPinCode] = useState<number>(0);
+	const [error, setError] = useState<string>('');
+	const navigate = useNavigate();
 
-  const getGameSession = async (pinCode: number) => {
-    try {
-      // get all game sessions
-      const gameSession = await DataStore.query(GameSession, (c) =>
-        c.and((c) => [c.pinCode.eq(pinCode)])
-      );
+	const getGameSession = async (pinCode: number) => {
+		try {
+			// get all game sessions
+			let gameSession = await DataStore.query(GameSession, (c) =>
+				c.and((c) => [c.pinCode.eq(pinCode)])
+			);
 
-      //const gameSessions = await DataStore.query(GameSession);
-      //console.log("gameSessions===", gameSessions);
-      if (gameSession.length === 0) return null;
-      return gameSession[0];
-    } catch (error) {
-      console.error(error);
-      return null;
-    }
-  };
+			// delay 0.5s
+			await new Promise((resolve) => setTimeout(resolve, 500));
 
-  const handlePinCodeChange = (e: any) => {
-    const pinCode = parseInt(e.target.value.trim(), 10);
-    setPinCode(pinCode);
-  };
+			// try again so that data is refreshed in underlay DataStore
+			gameSession = await DataStore.query(GameSession, (c) =>
+				c.and((c) => [c.pinCode.eq(pinCode)])
+			);
 
-  const handleJoinBtn = async () => {
-    const gameSession = await getGameSession(pinCode);
+			//const gameSessions = await DataStore.query(GameSession);
+			//console.log("gameSessions===", gameSessions);
+			if (gameSession.length === 0) return null;
+			return gameSession[0];
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	};
 
-    if (gameSession == null || gameSession.roundNumber > 0) {
-      setError("Invalid pin code or game session has already started");
-      return;
-    }
+	const handlePinCodeChange = (e: any) => {
+		const pinCode = parseInt(e.target.value.trim(), 10);
+		setPinCode(pinCode);
+	};
 
-    dispatch(setGameSessionID(gameSession.id));
-    console.log("Pin code in IF is ====", pinCode);
-    console.log("gameSession in IF is ====", gameSession);
+	const handleJoinBtn = async () => {
+		const gameSession = await getGameSession(pinCode);
 
-    // check redux store for user. For now just assuming user is new and needs to be created
-    try {
-      if (!userSessionID) {
-        const user = await DataStore.save(
-          new UserSession({
-            eliminated: false,
-            currentRoundResponse: "",
-            totalScore: 0,
-            totalGames: 0,
-            wins: 0,
-            losses: 0,
-            gameSessionID: gameSession.id,
-          })
-        );
+		if (gameSession == null || gameSession.roundNumber > 0) {
+			setError('Invalid pin code or game session has already started');
+			return;
+		}
 
-        dispatch(setUserSessionID(user.id));
-        console.log("IN IF ====", userSessionID);
-      } else {
-        console.log(userSessionID);
-        const user = await DataStore.query(UserSession, userSessionID);
+		dispatch(setGameSessionID(gameSession.id));
+		console.log('Pin code in IF is ====', pinCode);
+		console.log('gameSession in IF is ====', gameSession);
 
-        if (user == null) {
-          return;
-        }
+		// check redux store for user. For now just assuming user is new and needs to be created
+		try {
+			if (!userSessionID) {
+				const user = await DataStore.save(
+					new UserSession({
+						eliminated: false,
+						currentRoundResponse: '',
+						totalScore: 0,
+						totalGames: 0,
+						wins: 0,
+						losses: 0,
+						gameSessionID: gameSession.id
+					})
+				);
 
-        console.log("user ==", user);
+				dispatch(setUserSessionID(user.id));
+				console.log('IN IF ====', userSessionID);
+			} else {
+				console.log(userSessionID);
+				const user = await DataStore.query(UserSession, userSessionID);
 
-        await DataStore.save(
-          UserSession.copyOf(user, (updated) => {
-            updated.eliminated = false;
-            updated.currentRoundResponse = "";
-            updated.gameSessionID = gameSession.id;
-          })
-        );
-      }
+				if (user == null) {
+					return;
+				}
 
-      // update GameSession playerCount by 1
-      await DataStore.save(
-        GameSession.copyOf(gameSession, (updated) => {
-          updated.playerCount = gameSession.playerCount + 1;
-        })
-      );
+				console.log('user ==', user);
 
-      // go to waiting room
-      navigate(`/lobby`);
-    } catch (error: any) {
-      console.error(error);
-      setError(error.message);
-      console.log("ERROR IN TRY");
-    }
-  };
+				await DataStore.save(
+					UserSession.copyOf(user, (updated) => {
+						updated.eliminated = false;
+						updated.currentRoundResponse = '';
+						updated.gameSessionID = gameSession.id;
+					})
+				);
+			}
 
-  return (
-    <>
-      <LoginButton color="#000000" />
-      <h1>ARE YOU SMARTER THAN AN AI?</h1>
-      <div>Enter pin code to join</div>
-      <TextField
-        size="default"
-        placeholder="Pin code..."
-        onChange={handlePinCodeChange}
-      />
-      <Text style={{ color: "red" }}>{error}</Text>
-      <ToggleButton color="#62A1FF" onClick={handleJoinBtn}>
-        Join
-      </ToggleButton>
-    </>
-  );
+			// update GameSession playerCount by 1
+			await DataStore.save(
+				GameSession.copyOf(gameSession, (updated) => {
+					updated.playerCount = gameSession.playerCount + 1;
+				})
+			);
+
+			// go to waiting room
+			navigate(`/lobby`);
+		} catch (error: any) {
+			console.error(error);
+			setError(error.message);
+			console.log('ERROR IN TRY');
+		}
+	};
+
+	return (
+		<>
+			<LoginButton color="#000000" />
+			<h1>ARE YOU SMARTER THAN AN AI?</h1>
+			<div>Enter pin code to join</div>
+			<TextField
+				size="default"
+				placeholder="Pin code..."
+				onChange={handlePinCodeChange}
+			/>
+			<Text style={{ color: 'red' }}>{error}</Text>
+			<ToggleButton color="#62A1FF" onClick={handleJoinBtn}>
+				Join
+			</ToggleButton>
+		</>
+	);
 };
 
 export default JoinGame;
